6cb2dbac34ea4c8ed2a0cd00cc87630ecd1bc37c
Lucasi <lucaslipari06@gmail.com>
2025-06-19 12:57:51 +0200

    ajout React router

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,16 @@ import reactLogo from './assets/LOGO.png'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-
+import Home from './pages/Home'
 import Logements from './components/Logements';
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <Logements />
-    </div>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/test' element={<Logements/>}/>
+    </Routes>
   );
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,5 @@
+function Home(){
+    return <h1>Bienvenue à KASA</h1>
+}
+
+export default Home;
